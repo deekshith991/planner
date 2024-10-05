f2d313a630db50010e5df81e490d143f9a61fc6a
vaggu deekshith <vaggudeekshith@outlook.com>
2024-10-05 16:40:31 +0530

    Update: Add List Item input box is Done

--- student_app/src/Components/Sections/ListView.js
+++ student_app/src/Components/Sections/ListView.js
@@ -1,13 +1,18 @@
+// import { useState } from "react";
 
+import AddItem from "./AddItem";
 
 const ListView = () => {
+
+
+
 
   return (
     <div className=" max-w-full text-center bg-red-500 p-4">
       <h1 className=" text-4xl text-center ">list content</h1>
 
-      <button className="rounded-md p-4 bg-sky-300 w-44 m-4 " >ADD Item</button>
-    </div>
+      <AddItem />
+    </div >
   )
 }
 
