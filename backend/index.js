369e275e78f0c7290ea353b8c482d111ec8a3ab4
vaggu deekshith <vaggudeekshith@outlook.com>
2024-10-12 10:06:35 +0530

    Backend: mongo connection env setup

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,29 @@
+/*
+ * this is the Entry point to the server
+ * */
 
+// Getting the data from Environment variables
+const dotenv = require('dotenv');
+dotenv.config();
+
+// Modules required
+const mongoose = require('mongoose');
 
 console.log("[+] Starting Server");
 
+// Middleware
+
+// constants
+const MongoUri = process.env.MongoUrl || "mongodb://localhost:27017/planner";
+const db = mongoose.connection;
+
+
+
+// DataBase connection 
+mongoose.connect(MongoUri);
+db.once('error', () => {
+  console.log("[-] Error Connecting server \n\n");
+})
+db.on('open', () => {
+  console.log(`[+] Connected to DataBase ${MongoUri}`)
+})
