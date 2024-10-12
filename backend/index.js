8748b1fd68f43d555e772f30fcaa4a5de4402873
vaggu deekshith <vaggudeekshith@outlook.com>
2024-10-12 10:11:56 +0530

    Backend: Server Up

--- backend/index.js
+++ backend/index.js
@@ -8,12 +8,18 @@ dotenv.config();
 
 // Modules required
 const mongoose = require('mongoose');
+const express = require("express");
+const cors = require("cors");
 
 console.log("[+] Starting Server");
 
 // Middleware
+const app = express();
+app.use(express.json());
+app.use(cors());
 
 // constants
+const Port = process.env.PORT || 3333;
 const MongoUri = process.env.MongoUrl || "mongodb://localhost:27017/planner";
 const db = mongoose.connection;
 
@@ -21,9 +27,16 @@ const db = mongoose.connection;
 
 // DataBase connection 
 mongoose.connect(MongoUri);
+
 db.once('error', () => {
   console.log("[-] Error Connecting server \n\n");
 })
+
 db.on('open', () => {
-  console.log(`[+] Connected to DataBase ${MongoUri}`)
+  console.log(`[+] Connected to DataBase ${MongoUri}`);
+
+  app.listen(Port, () => {
+    console.log(`[+] Server Online @port ${Port}`);
+  });
+
 })
