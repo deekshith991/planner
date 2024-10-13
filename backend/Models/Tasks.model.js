dab0382bc475ced757ec51b7386ade005723aaab
vaggu deekshith <vaggudeekshith@outlook.com>
2024-10-13 11:38:48 +0530

    Backend: Basic Task schema done

---
+++ backend/Models/Tasks.model.js
@@ -0,0 +1,25 @@
+
+const mongoose = require("mongoose");
+
+const TaskSchema = mongoose.Schema({
+  Author: {
+    type: String,
+    require: true,
+  },
+  Uid: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Users",
+    require: true
+  },
+  UserId: {
+    type: String,
+    require: true
+  },
+  Task: {
+    type: String,
+    require: true
+  },
+
+});
+
+module.exports = mongoose.model("tasks", TaskSchema);
